9f95009f13f48305a1e0749a851b321a0aec9902
John Yun <johnyunjr@gmail.com>
2015-11-17 03:41:02 -0800

    email validation test?

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,15 +1,16 @@
 //test script for the signup form app
 
 describe('the signup app', function() {
-   
-    function addMultipleTasks(num) {
-        var idx;
-        for (idx = 0; idx < num; ++idx) {
-            addTask('Task ' + idx);
-        }
-    }
+    var emailInp = element(by.model('signup.email'));
+    var requiredEmail = $('.email-required-error');
 
     beforeEach(function() {
         browser.get('http://localhost:8080');
+    });
+
+    it('must show validation error for email', function () {
+        expect(requiredEmail.isPresent()).toEqual(false);
+        emailInp.sendKeys('abc');
+        expect(requiredEmail.isPresent()).toEqual(true);
     });
 });
