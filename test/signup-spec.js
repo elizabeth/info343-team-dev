6baa4feab17c619fba396a62de7e6093190de655
John Yun <johnyunjr@gmail.com>
2015-11-17 04:22:54 -0800

    fixed email tests

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -3,6 +3,7 @@
 describe('the signup app', function() {
     var emailInp = element(by.model('signup.email'));
     var requiredEmail = $('.email-required-error');
+    var invalidEmail = $('.email-invalid-error');
 
     beforeEach(function() {
         browser.get('http://localhost:8080');
@@ -12,20 +13,23 @@ describe('the signup app', function() {
         expect(browser.getTitle()).toEqual('Sign Up Form');
     });
 
-    it('must show validation error for email', function () {
+    it('must show required error for email', function () {
         expect(requiredEmail.isPresent()).toEqual(false);
         emailInp.sendKeys('abc');
+        emailInp.clear();
         expect(requiredEmail.isPresent()).toEqual(true);
+        emailInp.sendKeys('abc');
+        expect(requiredEmail.isPresent()).toEqual(false);
+        emailInp.clear();
     });
 
     it('must enter a valid email', function () {
-        expect(requiredEmail.isPresent()).toEqual(false);
+        expect(invalidEmail.isPresent()).toEqual(false);
         emailInp.sendKeys('youremail.com');
-        expect(requiredEmail.isPresent()).toEqual(true);
+        expect(invalidEmail.isPresent()).toEqual(true);
         emailInp.clear();
-        expect(requiredEmail.isPresent()).toEqual(false);
         emailInp.sendKeys('your@email.com');
-        expect(requiredEmail.isPresent()).toEqual(false);
+        expect(invalidEmail.isPresent()).toEqual(false);
         emailInp.clear();
     });
 });
