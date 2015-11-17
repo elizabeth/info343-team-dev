4063d2d2d79cba7b5c3659e8bd9504cd831d7bbb
luludust <elizabethduong96@gmail.com>
2015-11-17 14:38:23 -0800

    Fixing test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular.module('SignupApp', [])
-    // register a directive for custom validation of dates in the past
+    // register a directive for custom validation of at least being age 13
     .directive('moreThanThirteenAgo', function() {
         return {
             require: 'ngModel',
